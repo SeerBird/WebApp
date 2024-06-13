c9eb00addcaeeb8264b18a5181bf18bf266f16a3
SeerBird <ilya.govorunov@gmail.com>
2024-06-13 10:45:56 +0100

    Looked at socket.io, gonna try without it

--- go.mod
+++ go.mod
@@ -5,6 +5,12 @@ go 1.22.4
 require github.com/gin-gonic/gin v1.10.0
 
 require (
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
+
+require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -15,6 +21,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/googollee/go-socket.io v1.7.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,39 +1,51 @@
 package main
 
 import (
-	"net/http"
-
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/googollee/go-socket.io"
+	"net/http"
 )
 
+var Socketio_Server *socketio.Server
+
 func main() {
-	router := gin.Default()
-	// Define a route handler for the root path
+	var router = gin.Default()
+	Socketio_Server = socketio.NewServer(nil)
+
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Hello, You created a Web App!"})
 	})
+	router.Static("/static", "./static")
 
-	// Define a route handler for "/about"
-	router.GET("/about", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "About Us"})
-	})
-
-	// Define a route handler for "/contact"
-	router.GET("/contact", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Contact Us"})
-	})
+	router.GET("/socket.io", socketHandler)
+	router.POST("/socket.io", socketHandler)
+	router.Handle("WS", "/socket.io", socketHandler)
+	router.Handle("WSS", "/socket.io", socketHandler)
 
 	router.GET("/hello/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.JSON(http.StatusOK, gin.H{"message": "Hello, " + name + "!"})
 	})
-	// Serve static files
-	router.Static("/static", "./static")
-
 	// Define a route to render an HTML template
 	router.GET("/profile/:username", func(c *gin.Context) {
 		username := c.Param("username")
 		c.HTML(http.StatusOK, "profile.html", gin.H{"username": username})
 	})
 	router.Run(":8080")
+}
+
+func socketHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "Hello, You created a Web App!"})
+	Socketio_Server.OnConnect("connection", func(con socketio.Conn) error {
+		fmt.Println("on connection")
+		con.Join("chat")
+		return nil
+	})
+
+	Socketio_Server.OnError("error", func(so socketio.Conn, err error) {
+		fmt.Printf("[ WebSocket ] Error : %v", err.Error())
+	})
+
+	Socketio_Server.ServeHTTP(c.Writer, c.Request)
 }
