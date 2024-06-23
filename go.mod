12ed3746c8bad056500e235a4026bbeb1f13515e
SeerBird <ilya.govorunov@gmail.com>
2024-06-23 22:39:04 +0100

    Starting to try out structure ideas

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require github.com/gin-gonic/gin v1.10.0
 require (
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/gomodule/redigo v1.8.4 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/gorilla/websocket v1.5.2 // indirect
 )
 
 require (
--- main.go
+++ main.go
@@ -1,27 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/googollee/go-socket.io"
 	"net/http"
 )
 
-var Socketio_Server *socketio.Server
 
 func main() {
 	var router = gin.Default()
-	Socketio_Server = socketio.NewServer(nil)
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Hello, You created a Web App!"})
 	})
 	router.Static("/static", "./static")
-
-	router.GET("/socket.io", socketHandler)
-	router.POST("/socket.io", socketHandler)
-	router.Handle("WS", "/socket.io", socketHandler)
-	router.Handle("WSS", "/socket.io", socketHandler)
 
 	router.GET("/hello/:name", func(c *gin.Context) {
 		name := c.Param("name")
@@ -33,19 +24,4 @@ func main() {
 		c.HTML(http.StatusOK, "profile.html", gin.H{"username": username})
 	})
 	router.Run(":8080")
-}
-
-func socketHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "Hello, You created a Web App!"})
-	Socketio_Server.OnConnect("connection", func(con socketio.Conn) error {
-		fmt.Println("on connection")
-		con.Join("chat")
-		return nil
-	})
-
-	Socketio_Server.OnError("error", func(so socketio.Conn, err error) {
-		fmt.Printf("[ WebSocket ] Error : %v", err.Error())
-	})
-
-	Socketio_Server.ServeHTTP(c.Writer, c.Request)
 }
